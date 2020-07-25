33c461a5390909076e3fa5bdca030a6ecef73542
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-07-25 22:29:50 +0400

    new tasks

--- rotateImage.py
+++ rotateImage.py
@@ -5,6 +5,22 @@ def rotateImage(a):
             a[i][j], a[j][i] = a[j][i], a[i][j]
     return a
 
+'''
+def rotateImage(a):
+    return [ list(i) for i in list(zip(*a[::-1]))]
+
+'''
+
+'''
+def rotateImage(a):
+    a = list(zip(*a))
+    
+    for i in range(len(a)):
+        a[i] = a[i][::-1]    
+    
+    return a
+'''
+
 
 print(rotateImage([[1, 2, 3],
                    [4, 5, 6],

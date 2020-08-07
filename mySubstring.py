64beff950039a4be8af20efca8141c8566c42e23
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-08-07 18:38:54 +0400

    tasks

--- mySubstring.py
+++ mySubstring.py
@@ -4,3 +4,8 @@ def mySubstring(inputString, l, r):
     for i in range(l, r+1):
         result.append(inputString[i])
     return ''.join(result)
+
+
+# 2
+def mySubstring(inputString, l, r):
+    return inputString[l:r + 1]
---
+++ twoArraysNthElement.py
@@ -0,0 +1,34 @@
+def twoArraysNthElement(array1, array2, n):
+
+    def lowerBound(array, elem):
+        l = -1
+        r = len(array)
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if array[mid] <= elem:
+                l = mid
+            else:
+                r = mid
+        return l
+
+    l = -1
+    r = len(array1)
+
+    while l + 1 < r:
+        mid = (l + r) // 2
+        pos = lowerBound(array2, array1[mid])
+
+        if mid + pos + 1 <= n:
+            l = mid
+        else:
+            r = mid
+
+    if l > -1 and l + lowerBound(array2, array1[l]) + 1 == n:
+        return array1[l]
+    return twoArraysNthElement(array2, array1, n)
+
+
+
+print(twoArraysNthElement([1, 3, 4], [2, 6, 8], 5))
+
+

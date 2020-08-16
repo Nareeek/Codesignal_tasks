665bc51ba802d0893b60aeda9f30b88700133e27
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-08-16 23:22:48 +0400

    tasks

--- checkPalindrome.py
+++ checkPalindrome.py
@@ -7,3 +7,4 @@ def checkPalindrome(inputString):
             break
 
     return isPalindrome
+
--- makeArrayConsecutive.py
+++ makeArrayConsecutive.py
@@ -7,3 +7,15 @@ def makeArrayConsecutive(sequence):
             L.append(a + 1)
         a += 1
     return L
+
+
+
+# 2
+def makeArrayConsecutive(sequence):
+    M = max(sequence)
+    m = min(sequence)
+    res = []
+    for i in range(m, M):
+        if i not in sequence:
+            res.append(i)
+    return res

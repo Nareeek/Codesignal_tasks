108cf78b8dc0b671ff4d275d8ff299b82a2df230
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-08-17 22:13:43 +0400

    tasks

--- pagesNumberingWithInk.py
+++ pagesNumberingWithInk.py
@@ -7,3 +7,20 @@ def pagesNumberingWithInk(current, numberOfDigits):
 
 
 print(pagesNumberingWithInk(1, 5))
+
+
+# 2
+def pagesNumberingWithInk(current, numberOfDigits):
+
+    def countDigitsInNumber(n):
+        count = 0
+        while n > 0:
+            count += 1
+            n //= 10
+        return count
+    digitsInCurrent = countDigitsInNumber(current)
+    while numberOfDigits >= digitsInCurrent:
+        numberOfDigits -= digitsInCurrent
+        current += 1
+        digitsInCurrent = countDigitsInNumber(current)
+    return current - 1

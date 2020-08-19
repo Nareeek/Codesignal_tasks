0a49ca6a4b7ce97b0aa91cd22e7ff20e50dd2675
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-08-19 21:29:45 +0400

    tasks

--- chessBoardCellColor.py
+++ chessBoardCellColor.py
@@ -1,3 +1,20 @@
+# 1
+def chessBoardCellColor(cell1, cell2):
+
+    def getX(pos):
+        return ord(pos[0]) - ord('A')
+    def getY(pos):
+        return ord(pos[0]) - ord('1')
+
+    sum1 = getX(cell1[0]) + getY(cell1[1])
+    sum2 = getX(cell2[0]) + getY(cell2[1])
+    if sum1 % 2 == sum2 % 2:
+        return True
+    return False
+
+
+
+# 2
 def chessBoardCellColor(cell1, cell2):
     if ((ord(cell1[0]) % 2 == ord(cell2[0]) % 2) and (int(cell1[1]) % 2 == int(cell2[1]) % 2)):
         return True
--- chessBoardCellColor001.py
+++
@@ -1,6 +0,0 @@
-def chessBoardCellColor(cell1, cell2):
-    x = ord(cell1[0]) - ord(cell2[0])
-    y = ord(cell1[1]) - ord(cell2[1])
-    
-    return (x + y) % 2 == 0
-    

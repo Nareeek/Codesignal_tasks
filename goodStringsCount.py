2633bbef18f80d1b01ccd04864315389da18792c
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-08-21 22:14:20 +0400

    tasks

---
+++ goodStringsCount.py
@@ -0,0 +1,10 @@
+import operator as op
+def ncr(n, r):
+    r = min(r, n-r)
+    if r == 0: return 1
+    numer = reduce(op.mul, range(n, n-r, -1))
+    denom = reduce(op.mul, range(1, r+1))
+    return numer//denom
+
+def goodStringsCount(len):
+    return ncr(26,len) * (2**len - len-1)

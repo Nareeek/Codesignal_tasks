c5b5968e9357cc7fc352508ddb34d3ac658a850b
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-08-25 18:31:42 +0400

    tasks

--- largestDistance.py
+++ largestDistance.py
@@ -1,11 +1,13 @@
 def largestDistance(a):
-    Max = 0
 
+    mx = [a[0], a[1]]
+    mn = [a[1], a[0]]
     for i in range(len(a)):
-        for j in range(len(a)):
-            if i != j:
-                Max = max(Max, abs(a[i] - a[j]))
-    return Max
-
+        k = i % 2
+        if a[i] > mx[k]:
+            mx[k] = a[i]
+        elif a[i] < mn[k]:
+            mn[k] = a[i]
+    return max(mx[0] - mn[0], mx[1] - mn[1])
 
 print(largestDistance([0, 1, 1, 2]))

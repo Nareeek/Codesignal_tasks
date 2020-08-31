53473946ddc4e78692de45ac27b5004d52ad35e6
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-08-31 16:14:08 +0400

    tasks

--- maxSumSegments.py
+++ maxSumSegments.py
@@ -1,3 +1,4 @@
+# 1
 def maxSumSegments(inputArray):
     result = [0 for i in range(len(inputArray))]
     
@@ -15,4 +16,22 @@ def maxSumSegments(inputArray):
                 index = j - i + 1
         result[i - 1] = index    
 
+    return result
+
+# 2
+def maxSumSegments(inputArray):
+
+    result = []
+    for i in range(1, len(inputArray) + 1):
+        sum = 0
+        mxSum = 0
+        index = -1
+        for j in range(len(inputArray)):
+            sum += inputArray[j]
+            if j >= i:
+                sum -= inputArray[j - i]
+            if j >= i - 1 and (index == -1 or sum > mxSum):
+                mxSum = sum
+                index = j - i + 1
+        result.append(index)
     return result

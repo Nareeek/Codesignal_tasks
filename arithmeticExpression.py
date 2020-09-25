8cedd2d8c4ccf40bca4ac6d6eaba1040a5fe4f1a
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-09-25 14:08:29 +0400

    tasks

--- arithmeticExpression.py
+++ arithmeticExpression.py
@@ -1,3 +1,4 @@
+# 1
 def arithmeticExpression(a, b, c):
     r = False
     r |= a + b == c
@@ -6,6 +7,16 @@ def arithmeticExpression(a, b, c):
     r |= a / b == c
     return r
 
-
+# 2
 def arithmeticExpression(a, b, c):
-    return c in [a-b, a+b, a/b, a*b]`
+    return c in [a-b, a+b, a/b, a*b]
+
+
+# 3
+def arithmeticExpression(a, b, c):
+
+    if a + b == c or a * b == c or b * c == a or a - b == c:
+        return True
+    return False
+
+

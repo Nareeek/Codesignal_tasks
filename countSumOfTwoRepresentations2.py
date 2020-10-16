5887f42335713bd6adf664dd644996af32cb7374
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-10-16 22:58:27 +0400

    tasks

--- countSumOfTwoRepresentations2.py
+++ countSumOfTwoRepresentations2.py
@@ -1,3 +1,17 @@
+# 1
+def countSumOfTwoRepresentations(n, l, r):
+    result = 0
+
+    for a in range(l, r + 1):
+        b = a
+        while b <= r:
+            if a + b == n:
+                result += 1
+            b += 1
+
+    return result
+
+# 2
 def countSumOfTwoRepresentations2(n, l, r):
     dic = {n: n for n in range(l, r + 1)}
     s = set()
@@ -9,3 +23,10 @@ def countSumOfTwoRepresentations2(n, l, r):
 
 
 print(countSumOfTwoRepresentations2(6, 2, 4))
+
+
+
+# 3
+def countSumOfTwoRepresentations3(n, l, r):
+    return max(n // 2 - max(l, n - r) + 1, 0)
+

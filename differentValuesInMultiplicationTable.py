f099415931d683a5a73b8b8ef81d92aeb2c586f6
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-10-27 00:59:01 +0400

    tasks

--- differentValuesInMultiplicationTable.py
+++ differentValuesInMultiplicationTable.py
@@ -19,3 +19,13 @@ def differentValuesInMultiplicationTable(n, m):
                 result += 1
                 break
     return result
+
+# 3
+def differentValuesInMultiplicationTable2(n, m):
+    s = set()
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            s.add(i * j)
+        
+    return len(s)

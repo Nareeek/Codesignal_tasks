b6c6031845f1ff1d8a5e07bc59fea0b295d8c2ce
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-10-29 23:56:46 +0400

    tasks

--- isDigit.py
+++ isDigit.py
@@ -13,3 +13,8 @@ def isDigit(symbol):
     if '0' <= symbol and symbol <= '9':
         return True
     return False
+
+# 3
+def firstDigit(inputString):
+
+    return re.search('[0-9]', inputString).group(0)

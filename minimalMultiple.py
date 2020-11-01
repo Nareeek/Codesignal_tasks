983b358fa6ec461105641f0b3aa87b72530dcbaa
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-11-01 15:12:01 +0400

    tasks

--- minimalMultiple.py
+++ minimalMultiple.py
@@ -1,5 +1,14 @@
+# 1
 def minimalMultiple(divisor, lowerBound):
     while 1:
         if lowerBound % divisor == 0:
             return lowerBound
         lowerBound += 1
+        
+        
+# 2
+def minimalMultiple(divisor, lowerBound):
+
+    if lowerBound % divisor == 0:
+        return lowerBound
+    return lowerBound + (-lowerBound) % divisor

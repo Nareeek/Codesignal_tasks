1abd73db3bf18c7eb5338f0c6e467c2807a823bf
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-11-12 22:10:56 +0400

    tasks

--- visitsOnCircularRoad.py
+++ visitsOnCircularRoad.py
@@ -2,6 +2,6 @@ def visitsOnCircularRoad(n, visitsOrder):
     current = 1
     res = 0
     for i in range(len(visitsOrder)):
-        res += min(abs(visitsOrder[i] - current), n` - abs(visitsOrder[i] - current))
+        res += min(abs(visitsOrder[i] - current), n - abs(visitsOrder[i] - current))
         current = visitsOrder[i]
     return res

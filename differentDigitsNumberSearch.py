74aafa1b0d9744f3cea21fc89ef51b59526f3cf6
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-11-13 20:35:07 +0400

    tasks

--- differentDigitsNumberSearch.py
+++ differentDigitsNumberSearch.py
@@ -1,3 +1,24 @@
+# 1
+def differentDigitsNumberSearch(inputArray):
+
+    for i in range(len(inputArray)):
+        cur = inputArray[i]
+        was = [False] * 10
+        ok = True
+        while cur > 0:
+            if was[cur % 10]:
+                ok = False
+                break
+            was[cur % 10] = True
+            cur //= 10
+        if ok:
+            return inputArray[i]
+
+    return -1
+
+
+
+# 2
 def differentDigitsNumberSearch(inputArray):
     for a in inputArray:
         n = str(a)

c85dbe75e7c0aa38964acbc9a297e6de3f32548c
Nareeek <56166165+Nareeek@users.noreply.github.com>
2020-11-27 17:12:56 +0400

    tasks

--- nextSecond.py
+++ nextSecond.py
@@ -1,3 +1,20 @@
+# 0
+def nextSecond(currentTime):
+
+    if currentTime[2] == 59:
+        currentTime[2] = 0
+        if currentTime[1] == 59:
+            currentTime[1] = 0
+            currentTime[0] = (currentTime[0] + 1) % 24
+        else:
+            currentTime[1] += 1
+    else:
+        currentTime[2] += 1
+
+    return currentTime
+
+
+
 # 1
 def nextSecond(currentTime):
     x,y,z = currentTime[0],currentTime[1],currentTime[2]
